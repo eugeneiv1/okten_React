b2061622da205dddbc705d11a40ca48d09357528
Yevhenii <eugeneiv1@gmail.com>
2023-11-15 19:44:06 +0200

    replace Provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider} from "react-router-dom";
 import {router} from "./router";
+import {store} from "./store/store";
+import {Provider} from "react-redux";
 
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router} />
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 );
 
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,16 +1,15 @@
-import {Provider} from "react-redux";
-import {store} from "../store/store";
+
 import {Header} from "../components/Header/Header";
 import {Outlet} from "react-router-dom";
 
 
 const MainLayout = () => {
     return (
-        <Provider store={store}>
+        <div>
             <Header/>
             <hr/>
             <Outlet/>
-        </Provider>
+        </div>
     );
 };
 
